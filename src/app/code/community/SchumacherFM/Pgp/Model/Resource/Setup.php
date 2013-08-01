5ad79ffa93e2df1db47967556653e143eefa63e7
Cyrill Schumacher <Cyrill@Schumacher.fm>
2013-08-01 19:15:23 +1000

    add public key table

---
+++ src/app/code/community/SchumacherFM/Pgp/Model/Resource/Setup.php
@@ -0,0 +1,6 @@
+<?php
+
+class SchumacherFM_Pgp_Model_Resource_Setup extends Mage_Core_Model_Resource_Setup
+{
+
+}
--- src/app/code/community/SchumacherFM/Pgp/sql/pgp_setup/install-1.0.0.php
+++ src/app/code/community/SchumacherFM/Pgp/sql/pgp_setup/install-1.0.0.php
@@ -7,10 +7,31 @@
  * @license     http://www.gnu.org/licenses/gpl.html  GPL
  */
 
-/** @var Mage_Core_Model_Resource_Setup $installer */
 $installer = $this;
+/** @var SchumacherFM_Pgp_Model_Resource_Setup $installer */
 $installer->startSetup();
 
-// ...
+$table = $installer->getConnection()
+    ->newTable($installer->getTable('pgp/keys'))
+    ->addColumn('key_id', Varien_Db_Ddl_Table::TYPE_TEXT, 255, array(
+        'identity' => FALSE,
+        'nullable' => FALSE,
+        'primary'  => TRUE,
+    ), 'Key ID')
+    ->addColumn('email', Varien_Db_Ddl_Table::TYPE_TEXT, 255, array(
+        'unsigned' => TRUE,
+        'nullable' => FALSE,
+        'default'  => '0',
+    ), 'Email Address')
+    ->addColumn('public_key', Varien_Db_Ddl_Table::TYPE_TEXT, '64k', array(
+        'nullable' => FALSE,
+    ), 'PGP Public Key')
+    ->addColumn('creation_time', Varien_Db_Ddl_Table::TYPE_TIMESTAMP, null, array(), 'Creation Time')
+    ->addColumn('update_time', Varien_Db_Ddl_Table::TYPE_TIMESTAMP, null, array(), 'Modification Time')
+    ->addIndex(
+        $installer->getIdxName('pgp/keys', array('email')),
+        array('email'), array('type' => 'UNIQUE'))
+    ->setComment('PGP Public Key Table');
+$installer->getConnection()->createTable($table);
 
 $installer->endSetup();
